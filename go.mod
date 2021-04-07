2316569b40b426a3bd5c011f49b19988f98908e9
slumder <war7951@gmail.com>
2021-04-07 15:24:05 +0800

    add redis

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis/v8 v8.8.0
+	github.com/google/uuid v1.2.0
 	github.com/gorilla/websocket v1.4.2 // indirect
-	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376 // indirect
+	gopkg.in/olahol/melody.v1 v1.0.0-20170518105555-d52139073376
 )
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
 	"gopkg.in/olahol/melody.v1"
 )
 
@@ -14,17 +18,25 @@ type Message struct {
 	Content string `json:"content"`
 }
 
-func NewMessage(event, name, content string) *Message {
-	return &Message{
-		Event:   event,
-		Name:    name,
-		Content: content,
-	}
-}
+const (
+	KEY  = "chat_id"
+	WAIT = "wait"
+)
 
-func (m *Message) GetByteMessage() []byte {
-	result, _ := json.Marshal(m)
-	return result
+var redisClient *redis.Client
+
+func init() {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	pong, err := redisClient.Ping(context.Background()).Result()
+	if err == nil {
+		log.Println("redis 回應成功，", pong)
+	} else {
+		log.Fatal("redis 無法連線，錯誤為", err)
+	}
 }
 
 func main() {
@@ -44,22 +56,92 @@ func main() {
 	})
 
 	// 連線處理
+	// m.HandleConnect(func(session *melody.Session) {
+	// 	id := session.Request.URL.Query().Get("id")
+	// 	m.Broadcast(NewMessage("other", id, "加入聊天室").GetByteMessage())
+	// })
 	m.HandleConnect(func(session *melody.Session) {
-		id := session.Request.URL.Query().Get("id")
-		m.Broadcast(NewMessage("other", id, "加入聊天室").GetByteMessage())
+		id := InitSession(session)
+		if key, err := GetWaitFirstKey(); err == nil && key != "" {
+			CreateChat(id, key)
+			msg := NewMessage("other", "對方已經", "加入聊天室").GetByteMessage()
+			m.BroadcastFilter(msg, func(session *melody.Session) bool {
+				compareID, _ := session.Get(KEY)
+				return compareID == id || compareID == key
+			})
+		} else {
+			AddToWaitList(id)
+		}
 	})
 
 	// 離線處理
 	m.HandleClose(func(session *melody.Session, i int, s string) error {
-		id := session.Request.URL.Query().Get("id")
-		m.Broadcast(NewMessage("other", id, "離開聊天室").GetByteMessage())
-		return nil
+		// id := session.Request.URL.Query().Get("id")
+		// m.Broadcast(NewMessage("other", id, "離開聊天室").GetByteMessage())
+		// return nil
+		id := GetSessionID(session)
+		chatTo, _ := redisClient.Get(context.TODO(), id).Result()
+		msg := NewMessage("other", "對方已經", "離開聊天室").GetByteMessage()
+		RemoveChat(id, chatTo)
+		return m.BroadcastFilter(msg, func(session *melody.Session) bool {
+			compareID, _ := session.Get(KEY)
+			return compareID == chatTo
+		})
 	})
 
 	// 訊息處理
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
+		// m.Broadcast(msg)
+		id := GetSessionID(s)
+		chatTo, _ := redisClient.Get(context.TODO(), id).Result()
+		m.BroadcastFilter(msg, func(session *melody.Session) bool {
+			compareID, _ := session.Get(KEY)
+			return compareID == chatTo || compareID == id
+		})
 	})
 
 	r.Run(":5000")
+}
+
+func InitSession(s *melody.Session) string {
+	id := uuid.New().String()
+	s.Set(KEY, id)
+	return id
+}
+
+func GetSessionID(s *melody.Session) string {
+	if id, isExist := s.Get(KEY); isExist {
+		return id.(string)
+	}
+	return InitSession(s)
+}
+
+func GetWaitFirstKey() (string, error) {
+	return redisClient.LPop(context.Background(), WAIT).Result()
+}
+
+func AddToWaitList(id string) error {
+	return redisClient.LPush(context.Background(), WAIT, id).Err()
+}
+
+func CreateChat(id1, id2 string) {
+	redisClient.Set(context.Background(), id1, id2, 0)
+	redisClient.Set(context.Background(), id2, id1, 0)
+}
+
+func RemoveChat(id1, id2 string) {
+	redisClient.Del(context.Background(), id1, id2)
+}
+
+func NewMessage(event, name, content string) *Message {
+	return &Message{
+		Event:   event,
+		Name:    name,
+		Content: content,
+	}
+}
+
+func (m *Message) GetByteMessage() []byte {
+	result, _ := json.Marshal(m)
+	return result
 }
